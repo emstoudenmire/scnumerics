d4fa54504856d6166aa5e8e64e744430e2d0a69c
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2020-07-08 13:27:58 -0400

    Add generate.jl Spider driver code

---
+++ generate.jl
@@ -0,0 +1,17 @@
+using Spider
+
+let
+  args = (source_dir="src",
+          output_dir="docs",
+          clear_odir=false,
+          header_file="header.html",
+          footer_file="footer.html")
+
+  run_spider(BibTexRefs(),
+             ArxivLinks(),
+             TOC(),
+             WikiLinks(),
+             MathJax(),
+             BackLinks(),
+             ;args...)
+end
